60e47000b4aa0d3237769876db5ff04be1bf7611
Jon <jonathan.p.lamar@gmail.com>
2023-03-01 12:44:48 -0700

    Minor tweaks

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,31 +1,5 @@
 local wt = require("wezterm")
 
-local kanagawa = {
-	foreground = "#dcd7ba",
-	background = "#1f1f28",
-	cursor_bg = "#c8c093",
-	cursor_fg = "#c8c093",
-	cursor_border = "#c8c093",
-	selection_fg = "#c8c093",
-	selection_bg = "#2d4f67",
-	scrollbar_thumb = "#16161d",
-	split = "#16161d",
-	ansi = { "#090618", "#c34043", "#76946a", "#c0a36e", "#7e9cd8", "#957fb8", "#6a9589", "#c8c093" },
-	brights = { "#727169", "#e82424", "#98bb6c", "#e6c384", "#7fb4ca", "#938aa9", "#7aa89f", "#dcd7ba" },
-}
-
-local melange = {
-	foreground = "#ECE1D7",
-	background = "#292522",
-	cursor_bg = "#ECE1D7",
-	cursor_border = "#ECE1D7",
-	cursor_fg = "#292522",
-	selection_bg = "#403A36",
-	selection_fg = "#ECE1D7",
-	ansi = { "#1c1a18", "#b64b4e", "#78997A", "#E49B5D", "#7F91B2", "#B380B0", "#7B9695", "#C1A78E" },
-	brights = { "#867462", "#D47766", "#85B695", "#EBC06D", "#A3A9CE", "#CF9BC2", "#89B3B6", "#ECE1D7" },
-}
-
 function string.split(str, sep)
 	local t = {}
 	for s in string.gmatch(str, "([^" .. sep .. "]+)") do
@@ -49,14 +23,37 @@ wt.on("format-window-title", function(tab, pane, tabs, panes, config)
 end)
 
 return {
-	colors = melange,
-	force_reverse_video_cursor = true,
+	color_scheme = "Melange",
+	color_schemes = {
+		["Kanagawa"] = {
+			foreground = "#dcd7ba",
+			background = "#1f1f28",
+			cursor_bg = "#c8c093",
+			cursor_fg = "#c8c093",
+			cursor_border = "#c8c093",
+			selection_fg = "#c8c093",
+			selection_bg = "#2d4f67",
+			scrollbar_thumb = "#16161d",
+			split = "#16161d",
+			ansi = { "#090618", "#c34043", "#76946a", "#c0a36e", "#7e9cd8", "#957fb8", "#6a9589", "#c8c093" },
+			brights = { "#727169", "#e82424", "#98bb6c", "#e6c384", "#7fb4ca", "#938aa9", "#7aa89f", "#dcd7ba" },
+		},
+		["Melange"] = {
+			foreground = "#c9beb4",
+			background = "#292522",
+			cursor_bg = "#ECE1D7",
+			cursor_border = "#c9beb4",
+			cursor_fg = "#292522",
+			selection_bg = "#403A36",
+			selection_fg = "#c9beb4",
+			ansi = { "#1c1a18", "#b64b4e", "#78997A", "#E49B5D", "#7F91B2", "#B380B0", "#7B9695", "#C1A78E" },
+			brights = { "#867462", "#D47766", "#85B695", "#EBC06D", "#A3A9CE", "#CF9BC2", "#89B3B6", "#c9beb4" },
+		},
+	},
 	initial_cols = 100,
 	initial_rows = 50,
 	font = wt.font("JetBrainsMono Nerd Font"),
 	font_size = 14,
-	default_cursor_style = "BlinkingBar",
-	cursor_blink_rate = 500,
 	hide_tab_bar_if_only_one_tab = true,
 	window_background_opacity = 0.9,
 	text_background_opacity = 0.7,
